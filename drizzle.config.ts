e5e151a3b229d86838d47abb7f0418346a57011a
yogyy <yogimr261@gmail.com>
2025-06-04 13:12:39 +0700

    feat: add D1Database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
 export default defineConfig({
-	schema: './src/lib/server/db/schema.ts',
-	dialect: 'sqlite',
-	dbCredentials: { url: process.env.DATABASE_URL },
-	verbose: true,
-	strict: true
+  schema: './src/lib/server/db/schema.ts',
+  dialect: 'sqlite',
+  verbose: true,
+  strict: true,
+  driver: 'd1-http',
+  dbCredentials: {
+    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+    token: process.env.CLOUDFLARE_D1_TOKEN!
+  }
 });
